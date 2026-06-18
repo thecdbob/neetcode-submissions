f7a0e1ae8e792dd01be1ceb5fabcb92023e29c16
cdbob <thecdbob@gmail.com>
2026-06-18 09:04:39 -0700

    Add: minimum-window-with-characters - submission-6

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-6.py
@@ -0,0 +1,40 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(t) > len(s):
+            return ""
+        tDict = {}
+        minStart, minStop = -1, -1
+        minLength = None
+        #populate counts for each in t
+        for char in t:
+            tDict[char] = tDict.get(char, 0) + 1
+        #for this non ideal solution create a dictionary for s
+        #but don't populate it until the loop
+        
+        #iterate through s
+        #we are going to just get a O(n^2)*0m solution by double looping
+        # don't even include optimizations for the end of the array
+        # for - len t, just get it working
+        
+        l, r = 0, 0
+        sDict = {}
+        while r < len(s):
+            sDict[s[r]] = sDict.get(s[r], 0) + 1
+            while all(sDict.get(char, 0) >= count for char, count in tDict.items()):
+                if minLength is not None:
+                    if r-l < minStop-minStart:
+                        minStart, minStop = l, r
+                else:
+                    minStart, minStop = l, r
+                minLength = minStop-minStart
+                sDict[s[l]] = sDict.get(s[l], 0) - 1
+                l+=1
+            r+=1
+        if minLength is None:
+            return ""
+        else: return s[minStart:minStop+1]
+
+
+
+
+        
