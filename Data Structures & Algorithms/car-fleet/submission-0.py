6217e9b39ddbf2303446b91dd0b36c4ac842337c
cdbob <thecdbob@gmail.com>
2026-06-21 13:40:49 -0700

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        #O(NlogN) time to sort the array, reverse to sort by largest to
+        #smallest
+        sortedCars = sorted(zip(position, speed), reverse=True)
+        carStack = []
+        for pos, spd in sortedCars:
+            time = (target - pos) / spd
+            carStack.append(time)
+            if len(carStack) > 1 and time <= carStack[-2]:
+                carStack.pop()
+        return len(carStack)
+
